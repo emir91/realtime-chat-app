838ae5db64d1c0da2ce0068d7cc1b9a4b60e5bba
Emir Vranac <vranacemir@gmail.com>
2023-01-15 12:08:20 +0100

    Add socket to the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -8,8 +9,20 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+// Creates an io server and allow for CORS from 
+// http://localhost:3000 with GET and POST methods
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST']
+    }
+})
+
+// Listen for when the client connects via socket.io-client
+
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
 })
 
 server.listen(4000, () => {
